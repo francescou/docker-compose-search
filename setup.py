11a6d30ea3b0eca25f13ae849c42f8d76c8152af
Francesco Uliana <francesco@uliana.it>
2015-12-20 18:11:43 +0100

    bugfix

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='docker-compose-search',
 
-    version='1.0.0',
+    version='1.0.1',
 
     description='search docker-compose projects',
     long_description=long_description,
